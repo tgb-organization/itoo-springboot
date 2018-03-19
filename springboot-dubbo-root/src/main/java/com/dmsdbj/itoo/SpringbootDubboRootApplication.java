79bda64e4a586908413434e894eef2218ce6a7ec
zhengtao <15732621796@163.com>
2018-03-19 10:26:34 +0800

    springboot-dubbo-root/

---
+++ springboot-dubbo-root/src/main/java/com/dmsdbj/itoo/SpringbootDubboRootApplication.java
@@ -0,0 +1,12 @@
+package com.dmsdbj.itoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDubboRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDubboRootApplication.class, args);
+	}
+}
---
+++ springboot-dubbo-root/src/test/java/com/dmsdbj/itoo/SpringbootDubboRootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dmsdbj.itoo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDubboRootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
