be17a7c5bcc8c2a091389e7b95d6157af7a2c6b1
zhengtao <15732621796@163.com>
2018-03-19 08:52:01 +0800

    删除所有内容

--- springboot-dubbo-api/src/main/java/com/dmsdbj/itoo/facade/ItemFacade.java
+++
@@ -1,7 +0,0 @@
-package com.dmsdbj.itoo.facade;
-
-public interface ItemFacade {
-
-    //检查商品是否可售
-    boolean checkItemStatus(String id);
-}
--- springboot-dubbo-service/src/main/java/com/dmsdbj/itoo/service/ItemService.java
+++
@@ -1,6 +0,0 @@
-package com.dmsdbj.itoo.service;
-
-public interface ItemService {
-    //检查商品是否可售
-    boolean checkItemStatus(String id);
-}
